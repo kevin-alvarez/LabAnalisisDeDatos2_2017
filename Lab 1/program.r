bc06a4b267d87cb52c88a26d543784524767b003
Kevin Alvarez <kebingo12@hotmail.com>
2017-08-31 23:25:43 -0300

    Medidas tendencia central

--- Lab 1/program.r
+++ Lab 1/program.r
@@ -1,13 +1,40 @@
 
-rawdata <- read.csv("C:/Users/Chocho/Desktop/Analisis de Datos/Laboratorio/Lab 1/allhypo_filtered.csv", header=TRUE)
-data <- rawdata[(rawdata$TSH.measured=="t" & rawdata$T3.measured=="t" & rawdata$TT4.measured=="t" & rawdata$T4U.measured=="t" & rawdata$FTI.measured=="t"),]
+rawdata <- read.csv("C:/Users/Chocho/Desktop/Analisis de Datos/Laboratorio/Lab 1/allhypo_filtered.csv", header=TRUE, stringsAsFactors=FALSE)
+data <- rawdata[(rawdata$age!="?" & rawdata$TSH.measured=="t" & rawdata$T3.measured=="t" & rawdata$TT4.measured=="t" & rawdata$T4U.measured=="t" & rawdata$FTI.measured=="t"),]
 
 
 #Var Vectors
-tshlevels <- as.numeric(data$TSH)
-t3levels <- as.numeric(data$T3)
-t4ulevels <- as.numeric(data$T4U)
+age <- as.numeric(data$age)
+TSH <- as.numeric(data$TSH)
+T3 <- as.numeric(data$T3)
+TT4 <- as.numeric(data$TT4)
+T4U <- as.numeric(data$T4U)
+FTI <- as.numeric(data$FTI)
+
+mean_age <- mean(age)
+median_age <- median(age)
+var_age <- var(age)
+
+mean_TSH <- mean(TSH)
+median_TSH <- median(TSH)
+var_TSH <- var(TSH)
+
+mean_T3 <- mean(T3)
+median_T3 <- median(T3)
+var_T3 <- var(T3)
+
+mean_TT4 <- mean(TT4)
+median_TT4 <- median(TT4)
+var_TT4 <- var(TT4)
+
+mean_T4U <- mean(T4U)
+median_T4U <- median(T4U)
+var_T4U <- var(T4U)
+
+mean_FTI <- mean(FTI)
+median_FTI <- median(FTI)
+var_FTI <- var(FTI)
 
 #Plots
-hist(t3levels)
+#hist(t3levels)
 #mynewdata <- mydata[-c(1, 2, 3), ] - Delete data
