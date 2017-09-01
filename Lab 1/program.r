5c183627449c623edd1c370230030642a3be6fec
Marcela Rivera <marcela.rivera.c@usach.cl>
2017-09-01 03:30:36 -0300

    Moda de las variables
    se cambió la ruta del archivo.

--- Lab 1/program.r
+++ Lab 1/program.r
@@ -1,5 +1,5 @@
 
-rawdata <- read.csv("C:/Users/Chocho/Desktop/Analisis de Datos/Laboratorio/Lab 1/allhypo_filtered.csv", header=TRUE, stringsAsFactors=FALSE)
+rawdata <- read.csv("C:/Users/Marcela Rivera/Documents/GitHub/LabAnalisisDeDatos2_2017/Lab 1/allhypo_filtered.csv", header=TRUE, stringsAsFactors=FALSE)
 data <- rawdata[(rawdata$age!="?" & rawdata$TSH.measured=="t" & rawdata$T3.measured=="t" & rawdata$TT4.measured=="t" & rawdata$T4U.measured=="t" & rawdata$FTI.measured=="t"),]
 
 
@@ -14,27 +14,34 @@ FTI <- as.numeric(data$FTI)
 mean_age <- mean(age)
 median_age <- median(age)
 var_age <- var(age)
+moda_age <- mlv(age,method = "mfv")[1]
 
 mean_TSH <- mean(TSH)
 median_TSH <- median(TSH)
 var_TSH <- var(TSH)
+moda_TSH <- mlv(TSH,method = "mfv")[1]
 
 mean_T3 <- mean(T3)
 median_T3 <- median(T3)
 var_T3 <- var(T3)
+moda_T3 <- mlv(T3,method = "mfv")[1]
 
 mean_TT4 <- mean(TT4)
 median_TT4 <- median(TT4)
 var_TT4 <- var(TT4)
+moda_TT4 <- mlv(TT4,method = "mfv")[1]
 
 mean_T4U <- mean(T4U)
 median_T4U <- median(T4U)
 var_T4U <- var(T4U)
+moda_T4U <- mlv(T4U,method = "mfv")[1]
 
 mean_FTI <- mean(FTI)
 median_FTI <- median(FTI)
 var_FTI <- var(FTI)
+moda_FTI <- mlv(FTI,method = "mfv")[1]
 
 #Plots
 #hist(t3levels)
 #mynewdata <- mydata[-c(1, 2, 3), ] - Delete data
+
