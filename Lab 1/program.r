d323a51d783c59332b36efbcc44e800478d13708
Kevin Alvarez <kebingo12@hotmail.com>
2017-09-02 00:00:07 -0300

    Graficos con ggplot - coef correlacion con pearson

--- Lab 1/program.r
+++ Lab 1/program.r
@@ -1,7 +1,13 @@
 
-rawdata <- read.csv("C:/Users/Marcela Rivera/Documents/GitHub/LabAnalisisDeDatos2_2017/Lab 1/allhypo_filtered.csv", header=TRUE, stringsAsFactors=FALSE)
+rawdata <- read.csv("c:\\Users\\Chocho\\Desktop\\Analisis de Datos\\Laboratorio\\Lab 1\\allhypo_filtered.csv", header=TRUE, stringsAsFactors=FALSE)
 data <- rawdata[(rawdata$age!="?" & rawdata$TSH.measured=="t" & rawdata$T3.measured=="t" & rawdata$TT4.measured=="t" & rawdata$T4U.measured=="t" & rawdata$FTI.measured=="t"),]
 
+#Clean state
+d <- c()
+for(i in data$state){
+  d <- c(d, strsplit(i, ".", fixed = TRUE)[[1]][1])
+}
+data$state <- d
 
 #Var Vectors
 age <- as.numeric(data$age)
@@ -41,7 +47,30 @@ median_FTI <- median(FTI)
 var_FTI <- var(FTI)
 moda_FTI <- mlv(FTI,method = "mfv")[1]
 
-#Plots
-#hist(t3levels)
-#mynewdata <- mydata[-c(1, 2, 3), ] - Delete data
+#Plots (package: ggplot2)
+#Gráfico clasificacion de la muestra.
+p1 <- ggplot(data, aes(data$state))+geom_bar()+theme_bw()+scale_fill_brewer(palette = "Set1")
+
+#State vs sex
+p2 <- ggplot(data, aes(data$state, fill = data$sex))+geom_bar(position = position_dodge())+theme_bw()+scale_fill_brewer(palette = "Paired")
+
+#State vs pregnant
+p3 <- ggplot(data, aes(data$state, fill = data$pregnant))+geom_bar(position = position_dodge())+theme_bw()+scale_fill_brewer(palette = "Paired")
+
+#State vs thyroid.surgery
+p4 <- ggplot(data, aes(data$state, fill = data$thyroid.surgery))+geom_bar(position = position_dodge())+theme_bw()+scale_fill_brewer(palette = "Paired")
+
+#State vs I131.treatment
+p5 <- ggplot(data, aes(data$state, fill = data$I131.treatment))+geom_bar(position = position_dodge())+theme_bw()+scale_fill_brewer(palette = "Paired")
+
+#State vs lithium
+p6 <- ggplot(data, aes(data$state, fill = data$lithium))+geom_bar(position = position_dodge())+theme_bw()+scale_fill_brewer(palette = "Paired")
+
+#State vs goitre
+p7 <- ggplot(data, aes(data$state, fill = data$goitre))+geom_bar(position = position_dodge())+theme_bw()+scale_fill_brewer(palette = "Paired")
+
+
+#Correlation
+corData <- data.frame(age, TSH, T3, TT4, T4U, FTI)
+resCorrelate <- cor(corData, method = "pearson")
 
