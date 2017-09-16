81f5fa0e2e0b8f021c719e2e6ee051b46cebd7a3
kevin-alvarez <kebingo12@gmail.com>
2017-09-15 21:29:25 -0300

    dissimilarity with gower distance

--- Lab 2/program.r
+++ Lab 2/program.r
@@ -29,13 +29,42 @@ data$referral.source <- NULL
 #Delete clasification variable
 data$clasification <- NULL
 
-# Se limpian los datos de acuerdo a un rango de valores máximos
+#Data format transform
+#Nominal variables
+data$sex <- as.factor(data$sex)
+data$on.thyroxine <- as.factor(data$on.thyroxine)
+data$query.on.thyroxine <- as.factor(data$query.on.thyroxine)
+data$on.antithyroid.medication <- as.factor(data$on.antithyroid.medication)
+data$sick <- as.factor(data$sick)
+data$pregnant <- as.factor(data$pregnant)
+data$thyroid.surgery <- as.factor(data$thyroid.surgery)
+data$I131.treatment <- as.factor(data$thyroid.surgery)
+data$query.hypothyroid <- as.factor(data$query.hypothyroid)
+data$query.hyperthyroid <- as.factor(data$query.hyperthyroid)
+data$lithium <- as.factor(data$lithium)
+data$goitre <- as.factor(data$goitre)
+data$tumor <- as.factor(data$tumor)
+data$hypopituitary <- as.factor(data$hypopituitary)
+data$psych <- as.factor(data$psych)
+
+#Continuous variables
+data$age <- as.numeric(data$age)
+data$TSH <- as.numeric(data$TSH)
+data$T3 <- as.numeric(data$T3)
+data$TT4 <- as.numeric(data$TT4)
+data$T4U <- as.numeric(data$T4U)
+data$FTI <- as.numeric(data$FTI)
+
+# Data selection - set max value for detection of data anomalies
 TSHMAX <- 4.0 + abs(0.4 - 4.0) * 2
 T3MAX <-  3.37 + abs(1.07 - 3.37) * 2
 TT4MAX <- 164 + abs(64 - 164) * 2
 T4UMAX <- 1.8 + abs(0.7 - 1.8) * 2
 FTIMAX <- 1.8 + abs(33.108 - 135.191) * 2
-EstudioLimpiado <- subset(data, as.numeric(as.character(age)) <= 100 & (as.numeric(as.character(TSH)) <= TSHMAX) & (as.numeric(as.character(T3)) <= T3MAX) & (as.numeric(as.character(TT4)) <= TT4MAX) & (as.numeric(as.character(T4U)) <= T4UMAX) & (as.numeric(as.character(FTI)) <= FTIMAX))
+data <- subset(data, (age <= 100) & (TSH <= TSHMAX) & (T3 <= T3MAX) & (TT4 <= TT4MAX) & (T4U <= T4UMAX) & (FTI <= FTIMAX))
+
+#Calculating Dissimilarity with Gower Distance
+data_dist <- daisy(data, metric = "gower")
 
 
 #groups <- 6
